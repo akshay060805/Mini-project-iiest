4ad2dee55fc73e55f8efb8292a72b96b6ab16ccc
akshay060805 <vanama.akshay06@gmail.com>
2024-11-28 20:10:47 +0530

    ..

--- backend/models/addstartup.js
+++ backend/models/addstartup.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // mongoose.connect('mongodb://127.0.0.1:27017/startupsphere')
-mongoose.connect('mongodb+srv://vanamaakshay06:akki8464@startupsphere.skqyr.mongodb.net/?retryWrites=true&w=majority&appName=startupsphere/startupsphere')
+// mongoose.connect('mongodb+srv://vanamaakshay06:akki8464@startupsphere.skqyr.mongodb.net/?retryWrites=true&w=majority&appName=startupsphere/startupsphere')
 
 const addstartupSchema=mongoose.Schema({
     name:String,
